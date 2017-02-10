0021190433f5674ddffc80092e7453fb5e75f93c
Dave Voutila <voutilad@gmail.com>
2017-02-09 21:39:36 -0500

    readme stub.

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function inject_ta(taId) {
   var body = document.getElementsByTagName('body')[0];
   var script = document.createElement('script');
-  /*
-  <script> (function(a,b,c,d,e,f,g,h,i){i=/(^|;)\s*_tmc=([^;]+)/.exec(a[c]),i=i?i[2]:"",("done"!==i|| /[?&]_tmcf?(=|&|$)/.test(e))&&(g=a.getElementsByTagName(b)[0],h=a.createElement(b),h[d]("src",f+ "?r="+i+"&"+1*new Date),h[d]("type","text/javascript"),h.async=!0,g.parentNode.insertBefore(h,g)) })(document,"script","cookie","setAttribute",location.search,"//in.taskanalytics.com/00069/tm.js"); </script>
-  */
+
   script.type = 'text/javascript';
   script.text = 'alert("hey man! taId: ' + taId + '");';
   body.appendChild(script);
