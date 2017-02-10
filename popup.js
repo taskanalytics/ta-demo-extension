ef19035eb1cc355d3a403df8dcf4b18cc064bcfa
Dave Voutila <voutilad@gmail.com>
2017-02-09 20:38:25 -0500

    initial popup logic.

---
+++ popup.js
@@ -0,0 +1,28 @@
+// Saves options to chrome.storage
+function save_options() {
+  var taId = document.getElementById('taId').value;
+  console.log('Saving taID: ' + taId);
+  chrome.storage.sync.set({
+    taId: taId,
+  }, function() {
+    // Update status to let user know options were saved.
+    var msg = document.getElementById('msg');
+    msg.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+function restore_options() {
+  chrome.storage.sync.get({
+    taId: '',
+  }, function(items) {
+    document.getElementById('taId').value = items.taId;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  restore_options();
+  document.getElementById('update').addEventListener('click', save_options);
+});
