7959166ff9dfaf5b70d22c15c0ea2dccc4aa2c79
Dave Voutila <voutilad@gmail.com>
2017-02-14 11:01:49 -0500

    stubbed out mocha/chai support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,28 @@
 module.exports = function(grunt) {
-  
+
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    mocha: {
+      all: {
+        src: ['tests/testrunner.html']
+      },
+      options: {
+        run: true
+      }
+    },
     crx: {
       myPublicExtension: {
         src: "src/**/*",
         dest: "dist/myPublicExtension.zip",
       }
-
-      
-      //mySignedExtension: {
-      //  src: "src/**/*",
-      //  dest: "dist/myPrivateExtension.crx",
-      //  options: {
-      //    privateKey: "~/myPrivateExtensionKey.pem"
-      //  }
-      //}
-
     }
   });
 
+  grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-crx');
   grunt.loadNpmTasks('grunt-webpack');
 
   // Default task(s).
-  grunt.registerTask('default', ['crx']);
+  grunt.registerTask('default', ['mocha']);
+  grunt.registerTask('dist', ['mocha', 'crx'])
 };
